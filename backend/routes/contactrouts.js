819315c5649521c8b9a09468587b512ed010bcfa
student2683@web.de <student2683@web.de>
2024-01-13 21:20:14 +0100

    completed the controller to manage alone the data in the mongoDB

--- backend/routes/contactrouts.js
+++ backend/routes/contactrouts.js
@@ -1,59 +1,16 @@
 const express = require('express');
-const Contact = require('../model/contact')
-const mongoose = require("mongoose");
 const router = express.Router();
+const controller = require('../controller/contactcontroller')
 
-router.get('/', async (req, res) => {
-    const contacts = await Contact.find({});
-    res.json(contacts);
-})
+//route to the controller with specific exercise
+router.get('/', controller.getContacts);
 
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    if(!mongoose.isValidObjectId(id)){
-        return res.status(400).json({msg: "ID ist not valid."})
-    }
-    const contact = await Contact.findById(id);
-    res.json(contact);
-})
+router.get('/:id', controller.getContact)
 
-router.post('/', async (req, res) => {
-    const {Firstname, Lastname, Mobile} = req.body;
-    try {
-        const contactSaved = await Contact.create({Firstname, Lastname, Mobile});
-        res.json({msg: "New contact added."})
-    }catch (error){
-        res.status(400).json({msg: "New contact didn't added " + error});
-    }
+router.post('/', controller.createContact)
 
-})
+router.put('/:id', controller.changeContact)
 
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    if(!mongoose.isValidObjectId(id)){
-        return res.status(400).json({msg: "ID ist not valid."})
-    }
-    try {
-        const changedContact = await Contact.findByIdAndUpdate(id,{
-            ...req.body
-        });
-        res.json({msg: "Contact changed"});
-    }catch (error){
-        return res.json({msg: "Couldn't change the contact " + error})
-    }
-})
-
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    if(!mongoose.isValidObjectId(id)){
-        return res.status(400).json({msg: "ID ist not valid."})
-    }
-    try {
-        const contactSaved = await Contact.findByIdAndDelete(id);
-        return res.json({msg: "Contact cancled."})
-    }catch (error){
-        return res.json({msg: "Couldn't delete the contact."})
-    }
-})
+router.delete('/:id', controller.deleteContact)
 
 module.exports = router;
