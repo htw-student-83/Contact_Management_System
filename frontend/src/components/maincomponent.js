f2afbb1a2b526cc94483d01fecc9550fa137a8a3
student2683@web.de <student2683@web.de>
2024-01-13 22:19:26 +0100

    completed the first component

---
+++ frontend/src/components/maincomponent.js
@@ -0,0 +1,7 @@
+export default function Maincomponent(){
+    return(
+        <div className="flex flex-grow justify-center bg-green-300 p-11 text-4xl font-bold font-sans">
+            <h1>☎ Meine Kontakte</h1>
+        </div>
+    )
+}
