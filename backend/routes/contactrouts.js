7a35e8931a3099c1ae8f5d252b0cd614cc16e78e
student2683@web.de <student2683@web.de>
2024-01-27 15:51:04 +0100

    corrected the part of url to the controller

--- backend/routes/contactrouts.js
+++ backend/routes/contactrouts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/contactcontroller')
+const controller = require('../controller/contactController')
 
 router.get('/', controller.getContacts);
 
