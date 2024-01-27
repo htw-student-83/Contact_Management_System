375b09818d977ea05b2e31bab4186980d906058c
student2683@web.de <student2683@web.de>
2024-01-27 14:58:57 +0100

    corrected the style of login

--- frontend/src/components/contactForm.js
+++ frontend/src/components/contactForm.js
@@ -33,30 +33,36 @@ export default function ContactForm(){
     }
 
     return(
-        <div className="mt-20 ml-64">
+        <div className="mt-10 ml-72 min-w-96">
             <p className="font-bold text-2xl">Neuer Kontakt</p>
             <div className="">
-                <form onSubmit={handleSubmit} className="grid grid-cols-2 mt-5">
-                    <label className="mt-2">Firstname:</label>
-                    <input
-                        type="text"
-                        onChange={(event) => setFirstname(event.target.value)}
-                        value={Firstname}
-                        className="mr-20 p-2 font-mono"/>
-                    <label className="mt-7">Lastname:</label>
-                    <input
-                        type="text"
-                        onChange={(event) => setLastname(event.target.value)}
-                        value={Lastname}
-                        className="mr-20 p-2 mt-5  font-mono"/>
-                    <label className="mt-7">Mobile:</label>
-                    <input
-                        type="text"
-                        onChange={(event) => setMobile(event.target.value)}
-                        value={Mobile}
-                        className="mr-20 p-2 mt-6 font-mono"/>
+                <form onSubmit={handleSubmit} className="">
+                <div className="mt-10">
+                        <label className="mt-2">Vorname:</label>
+                        <input
+                            type="text"
+                            onChange={(event) => setFirstname(event.target.value)}
+                            value={Firstname}
+                            className="ml-5 p-2 w-72 font-mono"/>
+                    </div>
+                    <div className="mt-10">
+                        <label className="mt-7">Nachname:</label>
+                        <input
+                            type="text"
+                            onChange={(event) => setLastname(event.target.value)}
+                            value={Lastname}
+                            className="ml-2 p-2 w-72 font-mono"/>
+                    </div>
+                    <div className="mt-10">
+                        <label className="mt-7">Mobile:</label>
+                        <input
+                            type="text"
+                            onChange={(event) => setMobile(event.target.value)}
+                            value={Mobile}
+                            className="ml-10 p-2 w-72 font-mono"/>
+                    </div>
                     {Error && <div className="error">{Error.message}</div>}
-                    <button className="bg-neutral-300 mt-8 w-96 pt-2 pb-2 rounded-3xl">New contact</button>
+                    <button className="bg-neutral-300 w-96 mt-10 p-2 hover:bg-lime-400 hover:rounded-3xl font-sans font-bold">Neuer Kontakt</button>
                 </form>
             </div>
         </div>
--- frontend/src/components/contactdetails.js
+++ frontend/src/components/contactdetails.js
@@ -1,9 +1,9 @@
 export default function Contactdetails({contact}){
     return(
         <div className="font-serif text-lg mb-10 p-5 shadow-xl w-60 rounded-xl border-emerald-500 border">
-            <p>Firstname: {contact.Firstname}</p>
-            <p>Lastname: {contact.Lastname}</p>
-            <p>Mobile: {contact.Mobile}</p>
+            <p>{contact.Firstname}</p>
+            <p>{contact.Lastname}</p>
+            <p>{contact.Mobile}</p>
         </div>
     )
 }
