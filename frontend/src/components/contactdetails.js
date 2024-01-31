d09e1ec35535b3effa7b813e6b75c5c944aca10f
student2683@web.de <student2683@web.de>
2024-01-31 15:17:56 +0100

    working on the update method

--- frontend/src/components/contactdetails.js
+++ frontend/src/components/contactdetails.js
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {RiDeleteBin6Line} from "react-icons/ri";
+import { GrUpdate } from "react-icons/gr";
 
 export default function Contactdetails({contact}){
 
@@ -23,14 +24,34 @@ export default function Contactdetails({contact}){
         }
     }
 
+    const handleUpdateClick = async () =>{
+        const response = await fetch('/api/contact/' + contact._id, {
+            method: "DELETE"
+        })
+
+        const json = await response.json();
+
+        if(!response.ok){
+            setError(json.error);
+            console.log(error);
+        }
+
+        if(response.ok){
+            setError(null);
+            console.log("Contact was deleted.");
+        }
+    }
+
+
     return(
         <div className="grid grid-cols-2 font-serif text-lg mb-10 p-5 shadow-xl w-80 rounded-xl border-emerald-500 border">
             <div>
                 <p>{contact.Firstname} {contact.Lastname}</p>
                 <p>{contact.Mobile}</p>
             </div>
-            <div className="p-4 ml-20 hover: cursor-pointer">
-                <span onClick={handleDeleteClick}>{<RiDeleteBin6Line size={30}/>}</span>
+            <div className="flex flex-row p-4 ml-14 hover: cursor-pointer">
+                <span onClick={handleUpdateClick}>{<GrUpdate size={25}/>}</span>
+                <span className="ml-4" onClick={handleDeleteClick}>{<RiDeleteBin6Line size={25}/>}</span>
             </div>
         </div>
     )
