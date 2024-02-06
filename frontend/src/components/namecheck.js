a516626553442c51c27caf51f0f6c6549cf3e37f
student2683@web.de <student2683@web.de>
2024-02-06 01:11:14 +0100

    updated the filter function for updating a contact

--- frontend/src/components/namecheck.js
+++ frontend/src/components/namecheck.js
@@ -1,10 +1,16 @@
-export default function isValidName(newName) {
+const containsLetters = (newName) => {
     // Regular expression to match alphabetic characters, spaces, and some special characters
-    //TODO check what exacetly happen in line 4!
     var nameRegex = /^[A-Za-z\u00C0-\u024F\u1E00-\u1EFF' -]+$/;
-    var containsLetters = nameRegex.test(newName);
-    var specificSymbols = /\W/;
-    var without_specificSymbols = specificSymbols.test(newName);
-    return containsLetters && !without_specificSymbols;
-
+    return nameRegex.test(newName);
 }
+
+const contains_specific_symbols  = (newName) =>  {
+    var specificSymbols = /\W/;
+    return specificSymbols.test(newName);
+}
+
+module.exports = {
+    containsLetters,
+    contains_specific_symbols,
+}
+
